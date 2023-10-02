d622c80c39dee69967e12c86b3b5c179630b1217
ThisaraKavinda <thisara48kavinda@gmail.com>
2023-10-03 03:18:16 +0530

    feat: implemented manager filter

---
+++ Ryptor---Java-Servlet-main/src/java/ControllerHelper/ManagerFilter.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ControllerHelper;
+
+import java.io.IOException;
+import javax.jms.Session;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Thisara Kavinda
+ */
+public class ManagerFilter implements Filter{
+    
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {}
+  
+    @Override
+    public void doFilter(ServletRequest req,ServletResponse resp,
+                         FilterChain chain) throws IOException, ServletException {
+        
+        if (req instanceof HttpServletRequest) {
+            HttpServletRequest servletRequest = (HttpServletRequest)req;
+            HttpSession session = servletRequest.getSession();
+            Object userType = session.getAttribute("USER_TYPE");
+            if (userType.equals("manager") || userType.equals("admin")) {
+                chain.doFilter(req, resp);
+            } else {
+                req.getRequestDispatcher("Views/Admin/signin.jsp").forward(req, resp);
+            }
+        }
+    }
+    
+    @Override
+    public void destroy() {}
+}
+
+
